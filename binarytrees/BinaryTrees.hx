3b8e0a140a552488a862a2ed1d7afc8e0a39e4c5
YetClass <60978476+YetClass@users.noreply.github.com>
2020-05-31 08:30:57 +0000

    remove errors

---
+++ binarytrees/BinaryTrees.hx
@@ -0,0 +1,12 @@
+/* The Computer Language Benchmarks Game
+   http://shootout.alioth.debian.org/
+
+   contributed by Ian Martins
+*/
+
+import neko.Sys;
+import neko.Lib;
+
+class BinaryTrees
+{
+  
