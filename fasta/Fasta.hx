012eb4655c952b8930d1ccc5c32d5d1a3f96831e
YetClass <60978476+YetClass@users.noreply.github.com>
2020-07-12 10:20:19 +0000

    Update ubuntu_tools.md

---
+++ fasta/Fasta.hx
@@ -0,0 +1,81 @@
+
+/*
+  The Computer Language Benchmarks Game
+  http://shootout.alioth.debian.org/
+
+  contributed by Ian Martins
+*/
+class Fasta
+{
+  private var rnd : Float;
+
+  private var aluChar : String;
+
+  private var iubChar : String;
+  private var iubProb : Array<Float>;
+
+  private var homosapiensChar : String;
+  private var homosapiensProb : Array<Float>;
+
+  public function new()
+  {
+    rnd = 42;
+
+    aluChar = 'GGCCGGGCGCGGTGGCTCACGCCTGTAATCCCAGCACTTTGG'
+      + 'GAGGCCGAGGCGGGCGGATCACCTGAGGTCAGGAGTTCGAGA'
+      + 'CCAGCCTGGCCAACATGGTGAAACCCCGTCTCTACTAAAAAT'
+      + 'ACAAAAATTAGCCGGGCGTGGTGGCGCGCGCCTGTAATCCCA'
+      + 'GCTACTCGGGAGGCTGAGGCAGGAGAATCGCTTGAACCCGGG'
+      + 'AGGCGGAGGTTGCAGTGAGCCGAGATCGCGCCACTGCACTCC'
+      + 'AGCCTGGGCGACAGAGCGAGACTCCGTCTCAAAAA';
+
+    iubChar = 'acgtBDHKMNRSVWY';
+    iubProb = [0.27, 0.12, 0.12, 0.27, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02, 0.02];
+
+    homosapiensChar = 'acgt';
+    homosapiensProb = [0.3029549426680, 0.1979883004921, 0.1975473066391, 0.3015094502008];
+  }
+
+  public function run(nn)
+  {
+    neko.Lib.println('>ONE Homo sapiens alu');
+    repeatFasta(aluChar, nn*2);
+
+    neko.Lib.println('>TWO IUB ambiguity codes');
+    randomFasta(iubChar, iubProb, nn*3);
+
+    neko.Lib.println('>THREE Homo sapiens frequency');
+    randomFasta(homosapiensChar, homosapiensProb, nn*5);
+  }
+
+  public function repeatFasta(src:String, nn:Int)
+  {
+    var width = 60;
+    var rr = src.length;
+    var ss = src+src+src.substr(0,nn%rr);
+    var ii = 0;
+    for( jj in 0...Std.int(nn/width) )
+    {
+      ii = (jj*width) % rr;
+      neko.Lib.println(ss.substr(ii, width));
+    }
+
+    if( (nn%width) != 0 )
+      neko.Lib.println(ss.substr(-(nn%width)));
+  }
+
+  public function randomFasta(tableChar, tableProb, nn)
+  {
+    var width = 60;
+    var probList = makeCumulative(tableProb);
+    var buf = new StringBuf();
+    for( ii in 0...nn )
+    {
+      buf.add(tableChar.charAt(bisect(probList, genRandom())));
+      if( (ii+1)%width == 0 )
+      {
+	neko.Lib.println(buf.toString());
+	buf = new StringBuf();
+      }
+    }
+    if( nn%width != 0 )
