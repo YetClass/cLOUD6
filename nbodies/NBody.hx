519c88bde6e91e5873c3791ad15f748a24aaad50
YetClass <60978476+YetClass@users.noreply.github.com>
2020-07-19 09:50:02 +0000

    chore: Publish

--- nbodies/NBody.hx
+++ nbodies/NBody.hx
@@ -2,4 +2,12 @@
  * The Computer Language Benchmarks Game
  * http://shootout.alioth.debian.org/
  *
- * Transl
+ * Translated from Christoph Bauer's nbody.c by Ian Martins
+ */
+
+typedef Planet = {
+    var x :Float;
+    var y :Float;
+    var z :Float;
+    var vx :Float;
+    var vy :F
